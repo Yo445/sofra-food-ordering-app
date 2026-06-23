5a40ed3f33d3b192e988bf71a01194431c78d48a
Yo445 <ya108942@gmail.com>
2026-06-23 15:06:49 +0300

    Enhance server startup logging and open Swagger documentation automatically
    - Added logging for the Swagger documentation URL upon server startup.
    - Implemented automatic opening of the Swagger documentation in the default browser based on the operating system.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import app from "./app";
 import { env } from "./config/env";
 import { connectDB } from "./config/db";
@@ -7,7 +8,18 @@ async function main() {
   await connectDB();
 
   app.listen(env.port, () => {
-    logger.info(`Server running on http://localhost:${env.port}`);
+    const url = `http://localhost:${env.port}`;
+    const docsUrl = `${url}/api-docs`;
+    logger.info(`Server running on ${url}`);
+    logger.info(`Swagger docs at ${docsUrl}`);
+
+    const cmd =
+      process.platform === "darwin"
+        ? `open ${docsUrl}`
+        : process.platform === "win32"
+          ? `start "" "${docsUrl}"`
+          : `xdg-open ${docsUrl}`;
+    exec(cmd);
   });
 }
 
